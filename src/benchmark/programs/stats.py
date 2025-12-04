98dd6de7ab39559ecde75eab26cffca2c1071b4b
c2fc2f <contact@c2fc2f.com>
2025-12-04 09:39:26 +0100

    feat(benchmark/stats): merge line stats about rag

--- src/benchmark/programs/stats.py
+++ src/benchmark/programs/stats.py
@@ -118,19 +118,13 @@ def display_one_stats(info: Stats, ind: int) -> None:
             print(
                 f"     Errors     - Mean: {mean(errors):.2f} | Median: {median(errors):.2f}"
             )
-            print(
-                f"     Nodes      - Mean: {mean(nodes):.2f} | Median: {median(nodes):.2f}"
-            )
             with_nodes = sum(1 for n in nodes if n >= 1)
             print(
-                f"                - Nodes ≥ 1: {with_nodes}/{len(nodes)} ({with_nodes / len(nodes) * 100:.1f}%)"
-            )
-            print(
-                f"     Edges      - Mean: {mean(edges):.2f} | Median: {median(edges):.2f}"
+                f"     Nodes      - Mean: {mean(nodes):.2f} | Median: {median(nodes):.2f} | ≥ 1: {with_nodes}/{len(nodes)} ({with_nodes / len(nodes) * 100:.1f}%)"
             )
             with_edges = sum(1 for n in edges if n >= 1)
             print(
-                f"                - Edges ≥ 1: {with_edges}/{len(edges)} ({with_edges / len(edges) * 100:.1f}%)"
+                f"     Edges      - Mean: {mean(edges):.2f} | Median: {median(edges):.2f} | ≥ 1: {with_edges}/{len(edges)} ({with_edges / len(edges) * 100:.1f}%)"
             )
             print()
 
