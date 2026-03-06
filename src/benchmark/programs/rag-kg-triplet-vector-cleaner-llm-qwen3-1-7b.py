88a46dbf16abc525e69390443ca517867c2d6f51
c2fc2f <contact@c2fc2f.com>
2026-03-06 07:32:46 +0100

    feat(rag-kg-triplet-vector-cleaner-llm-qwen3-1-7b): use top 5 nodes

--- src/benchmark/programs/rag-kg-triplet-vector-cleaner-llm-qwen3-1-7b.py
+++ src/benchmark/programs/rag-kg-triplet-vector-cleaner-llm-qwen3-1-7b.py
@@ -108,7 +108,7 @@ def base_grahygie(choices_keys: list[str]) -> tuple[Graph, Chattable]:
     retrieval_vector: Chattable = Vector2Cypher(
         index="CUI_EMBEDDINGS",
         embedder=embedder,
-        top_k=2,
+        top_k=5,
         distance=1,
     )
 
